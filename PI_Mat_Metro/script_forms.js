bc41524d90e306bc3d3898c4d4d29737d2d14037
guiambrogini <161897213+guiambrogini@users.noreply.github.com>
2024-11-17 19:13:38 -0300

    Update script_forms.js

--- PI_Mat_Metro/script_forms.js
+++ PI_Mat_Metro/script_forms.js
@@ -6,3 +6,11 @@ document.getElementById('forms-contato').addEventListener('submit', function(eve
     
     document.getElementById('forms-contato').reset();
   });
+  
+  function mudarTexto3(element) {
+    element.innerText = "Faça Parte >>"; 
+}
+
+function resetarTexto3(element) {
+    element.innerText = "Faça Parte"; 
+}
